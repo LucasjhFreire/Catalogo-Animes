dd79eb1055806b8daa869825f70ffe6c42fafab9
LucasjhFreire <lucasjhadielfreire04072008@gmail.com>
2026-09-15 11:23:41 -0300

    feat: criação do projeto e header footer e anime-card

---
+++ src/app/anime-card/anime-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-anime-card',
+  styleUrl: './anime-card.css',
+  templateUrl: './anime-card.html',
+})
+export class AnimeCard {}
