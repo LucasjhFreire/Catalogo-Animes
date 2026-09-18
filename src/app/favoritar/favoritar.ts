f4cf0e186f7d305f0357720c4820b56ae4ae14e8
LucasjhFreire <lucasjhadielfreire04072008@gmail.com>
2026-09-18 11:30:57 -0300

    feat: criação da interface Anime

---
+++ src/app/favoritar/favoritar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-favoritar',
+  styleUrl: './favoritar.css',
+  templateUrl: './favoritar.html',
+})
+export class Favoritar {}
---
+++ src/app/mostrar-detalhes/mostrar-detalhes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-mostrar-detalhes',
+  styleUrl: './mostrar-detalhes.css',
+  templateUrl: './mostrar-detalhes.html',
+})
+export class MostrarDetalhes {}
